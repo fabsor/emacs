1c122f7a5c6ee39d524b7e93ecf54b634c350272
Fabian Sorqvist <fabian@sorqvist.nu>
2013-12-13 07:21:58 +0100

    Add drupal mode and some default settings

---
+++ personal/personal.el
@@ -0,0 +1,14 @@
+;;; Summary
+
+(setq prelude-guru nil)
+
+(custom-set-variables
+ '(scroll-bar-mode nil))
+
+(add-to-list 'auto-mode-alist '("\\.php$" . drupal-mode))
+(add-to-list 'auto-mode-alist '("\\.module$" . drupal-mode))
+(add-to-list 'auto-mode-alist '("\\.test$" . drupal-mode))
+(add-to-list 'auto-mode-alist '("\\.inc$" . drupal-mode))
+(add-to-list 'auto-mode-alist '("\\.install$" . drupal-mode))
+(add-to-list 'auto-mode-alist '("\\.profile$" . drupal-mode))
+(add-to-list 'auto-mode-alist '("\\.engine$" . drupal-mode))
